ebc5d5c6f6827f69d392f1d5b63d4b4466ba5f56
Khalid Bin Ahsan <khalid.freelancerbd@gmail.com>
2024-04-01 04:57:34 +0600

    Final changes

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,7 +5,7 @@
 <template>
 <section class="container mx-auto min-h-[70vh] max-w-screen-xl flex items-center justify-center">
 <div>
-    <h3 class="text-3xl font-bold text-[#824A31] text-center italic">Welcome to CoffeChat</h3>
+    <h3 class="text-3xl font-bold text-[#824A31] text-center italic mt-5 md:mt-2">Welcome to CoffeChat</h3>
     <div class="md:flex">
     <img class="max-w-[300px]" src="https://parspng.com/wp-content/uploads/2022/12/coffeepng.parspng.com_.png" alt="coffee cup">
     <img class="max-w-[300px]" src="https://png.pngtree.com/png-clipart/20230430/original/pngtree-black-ceramic-coffee-mug-png-image_9124456.png" alt="coffee cup">
